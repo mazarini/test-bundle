2826cf3203b89f8e8ff3693c713fd29469da3cec
mazarini <mazarini@protonmail.com>
2020-02-11 13:56:21 +0100

    Suppress Doctrine from config Only use repository as parent for a fake repository

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,22 @@
 <?php
 
+/*
+ * Copyright (C) 2019 Mazarini <mazarini@protonmail.com>.
+ * This file is part of mazarini/test-bundle.
+ *
+ * mazarini/test-bundle is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * mazarini/test-bundle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ */
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Mazarini\PackageBundle\MazariniPackageBundle::class => ['all' => true],
@@ -10,6 +27,4 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
