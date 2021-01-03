43956b39b113459fed60c30777b7f42a4e264e7d
Mazarini <mazarini@mazarini.ovh>
2021-01-03 13:56:46 +0100

    Fix some warning

--- public/index.php
+++ public/index.php
@@ -1,22 +1,5 @@
 <?php
 
-/*
- * Copyright (C) 2019-2020 Mazarini <mazarini@protonmail.com>.
- * This file is part of mazarini/model.
- *
- * mazarini/model is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * mazarini/model is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License.
- */
-
 use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
@@ -24,31 +7,12 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (Kernel::isOlder(50100)) {
-    $dotEnv = new Dotenv();
-    $method = 'bootEnv';
-} else {
-    $dotEnv = new Dotenv(false);
-    $method = 'loadEnv';
-}
-if (method_exists($dotEnv, $method)) {
-    $dotEnv->$method(dirname(__DIR__).'/.env');
-} else {
-    throw new LogicException(sprintf('Unknow method "%s::%s" : env not initialized', get_class($dotEnv), $method));
-}
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
     Debug::enable();
-}
-
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
